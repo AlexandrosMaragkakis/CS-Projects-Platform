1e873535fe596ec6800cf15c59c9f66e0cd21ea6
Alexandros Maragkakis <maragkakis.alexandros@gmail.com>
2024-11-10 15:41:05 +0200

    Remove project relationship on Student model so it can work

--- app/blueprints/auth/models.py
+++ app/blueprints/auth/models.py
@@ -9,7 +9,6 @@ from neomodel import (
 from flask_login import UserMixin
 
 
-# only the student for now
 class User(StructuredNode, UserMixin):
     """Database model for Neo4j"""
 
@@ -17,10 +16,6 @@ class User(StructuredNode, UserMixin):
     email = EmailProperty(unique_index=True)
     password_hash = StringProperty()
     full_name = StringProperty()
-    # role = StringProperty(default="student")
-
-    # skilled_in_topics = RelationshipTo("Topic", "SKILLED_IN")
-    # worked_in_projects = RelationshipTo("Project", "WORKED_IN")
 
     @staticmethod
     def get_by_id(user_id):
@@ -51,7 +46,7 @@ class User(StructuredNode, UserMixin):
 class Student(User):
     github_username = StringProperty()
     github_token = StringProperty()
-    worked_in_projects = RelationshipTo("Project", "WORKED_IN")
+    # worked_in_projects = RelationshipTo("Project", "WORKED_IN")
     # interested_topics = RelationshipTo("Topic", "INTERESTED_IN")
     # skilled_topics = RelationshipTo("Topic", "SKILLED_IN")
     pass
